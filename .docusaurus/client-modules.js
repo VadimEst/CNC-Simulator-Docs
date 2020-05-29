5264256b3e09b7a4451d2019ef5a7bfa341b6aa1
YannikPfrengle <65347502+YannikPfrengle@users.noreply.github.com>
2020-05-29 12:13:08 +0200

    intial setup Yannik

--- .docusaurus/client-modules.js
+++ .docusaurus/client-modules.js
@@ -1,7 +1,7 @@
 export default [
-  require("C:\\Users\\i520940\\Documents\\GitHub\\CNC-Simulator-Docs\\node_modules\\remark-admonitions\\styles\\infima.css"),
-  require("C:\\Users\\i520940\\Documents\\GitHub\\CNC-Simulator-Docs\\node_modules\\remark-admonitions\\styles\\infima.css"),
-  require("C:\\Users\\i520940\\Documents\\GitHub\\CNC-Simulator-Docs\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("C:\\Users\\i520940\\Documents\\GitHub\\CNC-Simulator-Docs\\node_modules\\@docusaurus\\theme-classic\\src\\prism-include-languages"),
-  require("C:\\Users\\i520940\\Documents\\GitHub\\CNC-Simulator-Docs\\src\\css\\custom.css"),
+  require("C:\\Users\\i518157\\Documents\\GitHub\\CNC-Simulator-Docs\\node_modules\\remark-admonitions\\styles\\infima.css"),
+  require("C:\\Users\\i518157\\Documents\\GitHub\\CNC-Simulator-Docs\\node_modules\\remark-admonitions\\styles\\infima.css"),
+  require("C:\\Users\\i518157\\Documents\\GitHub\\CNC-Simulator-Docs\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("C:\\Users\\i518157\\Documents\\GitHub\\CNC-Simulator-Docs\\node_modules\\@docusaurus\\theme-classic\\src\\prism-include-languages"),
+  require("C:\\Users\\i518157\\Documents\\GitHub\\CNC-Simulator-Docs\\src\\css\\custom.css"),
 ];
